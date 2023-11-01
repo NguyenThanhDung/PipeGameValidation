ec28d5d966e4c56193657f60244adf68ab500cb5
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-01 10:56:44 -0500

    Make method be non-static

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -7,10 +7,12 @@
          "0b27275100000",
          "00c7256500000",
          "0006A45000000"};
-        Console.WriteLine(solution(state) == 19);
+
+        Program program = new Program();
+        Console.WriteLine(program.solution(state) == 19);
     }
 
-    public static int solution(string[] state)
+    public int solution(string[] state)
     {
         return 0;
     }
