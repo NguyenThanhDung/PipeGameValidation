f24eef77e07f1b5f49bb7264753b4d747c0dd40c
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-01 10:20:53 -0500

    Add test case

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -2,6 +2,16 @@
 {
     public static void Main()
     {
-        Console.WriteLine("Hello world");
+        string[] state = {"a224C22300000",
+         "0001643722B00",
+         "0b27275100000",
+         "00c7256500000",
+         "0006A45000000"};
+        Console.WriteLine(solution(state) == 19);
+    }
+
+    public static int solution(string[] state)
+    {
+        return 0;
     }
 }
