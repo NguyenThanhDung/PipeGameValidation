a4001e7e52a7ee8b8992b8fd87463b2a72d3737e
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-01 11:29:31 -0500

    Fix bug: Did not parse the end of the row

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -70,7 +70,7 @@
 
         for (int i = 0; i < state.Length; i++)
         {
-            for (int j = 0; j < state.Length; j++)
+            for (int j = 0; j < state[i].Length; j++)
             {
                 if (state[i][j] != '0')
                 {
