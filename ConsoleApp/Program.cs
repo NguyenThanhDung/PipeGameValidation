48b22f3f67c7d28e9f745be3ea041ad99796e35b
Dung Nguyen Thanh <dung.nguyenthanh@hotmail.com>
2023-11-01 11:04:06 -0500

    Add Pipe class

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,39 @@
 ﻿public class Program
 {
+    public enum PipeType
+    {
+        Vertical = 1,
+        Horizontal,
+        BottomRight,
+        BottomLeft,
+        TopLeft,
+        TopRight,
+        Cross
+    }
+
+    public class Pipe
+    {
+        private PipeType type;
+
+        private Pipe topAdjacentPipe;
+        private Pipe bottomAdjacentPipe;
+        private Pipe leftAdjacentPipe;
+        private Pipe rightAdjacentPipe;
+        private bool hasWater;
+
+        public Pipe(PipeType type)
+        {
+            this.Type = type;
+        }
+
+        public PipeType Type { get => type; set => type = value; }
+        public Pipe TopAdjacentPipe { get => topAdjacentPipe; set => topAdjacentPipe = value; }
+        public Pipe BottomAdjacentPipe { get => bottomAdjacentPipe; set => bottomAdjacentPipe = value; }
+        public Pipe LeftAdjacentPipe { get => leftAdjacentPipe; set => leftAdjacentPipe = value; }
+        public Pipe RightAdjacentPipe { get => rightAdjacentPipe; set => rightAdjacentPipe = value; }
+        public bool HasWater { get => hasWater; set => hasWater = value; }
+    }
+
     public static void Main()
     {
         string[] state = {"a224C22300000",
